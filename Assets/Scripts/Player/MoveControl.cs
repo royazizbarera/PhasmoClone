743fc022c225318589f81324026abe6c9664e4cd
Maxx31 <maxym.619@ukr.net>
2022-08-06 17:52:59 +0300

    Move Control

--- Assets/Scripts/Player/MoveControl.cs
+++ Assets/Scripts/Player/MoveControl.cs
@@ -11,20 +11,36 @@ public class MoveControl : MonoBehaviour
     private float _curSpeedMultiplier = 1f;
 
     private InputSystem _inputSystem;
+
+    private float _xMove, _zMove;
+    private bool _isSprinting;
+
     private void Awake()
     {
-        _inputSystem = AllServices.Container.Single<InputSystem>();
+        //_inputSystem = AllServices.Container.Single<InputSystem>();
     }
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        _xMove = Input.GetAxis("Horizontal");
+        _zMove = Input.GetAxis("Vertical");
         bool sprint = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 move = CalculateMove(sprint);
+
+        controller.Move(move * _speed * _curSpeedMultiplier * Time.deltaTime);
+    }
+
+
+    private Vector3 CalculateMove(bool sprint)
+    {
         if (sprint) _curSpeedMultiplier = _sprintMultiplier;
 
         else _curSpeedMultiplier = 1f;
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * _speed * _curSpeedMultiplier * Time.deltaTime);
+        Vector3 result = transform.right * _xMove + transform.forward * _zMove;
+        return result;
     }
+
+
+
 }
