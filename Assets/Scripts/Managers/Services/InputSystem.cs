372655fd93a7bc1fc4d3a529f9dc111b67a01c29
Maxx31 <maxym.619@ukr.net>
2022-08-09 16:10:19 +0300

    Lock управления

--- Assets/Scripts/Managers/Services/InputSystem.cs
+++ Assets/Scripts/Managers/Services/InputSystem.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class InputSystem : MonoBehaviour, IService
 {
+
+    public bool IsControlLocked = false;
     public Vector2 Axis { get; private set; }
     public Vector2 CameraAxis { get; private set; }
     public bool IsRunning = false;
@@ -23,11 +25,29 @@ public class InputSystem : MonoBehaviour, IService
 
     private void Update()
     {
-        CameraAxis = _mainInputAction.Player.Camera.ReadValue<Vector2>();
-        Axis = _mainInputAction.Player.Movement.ReadValue<Vector2>();
-        IsRunning = _mainInputAction.Player.Run.ReadValue<float>() == 1 ? true : false;
+        ReadControl();
     }
 
+
+    private void ReadControl()
+    {
+        if (!IsControlLocked)
+        {
+            CameraAxis = _mainInputAction.Player.Camera.ReadValue<Vector2>();
+            Axis = _mainInputAction.Player.Movement.ReadValue<Vector2>();
+            IsRunning = _mainInputAction.Player.Run.ReadValue<float>() == 1 ? true : false;
+        }
+    }
+
+    private void LockControl()
+    {
+        IsControlLocked = true;
+    } 
+    
+    private void UnLockControl()
+    {
+        IsControlLocked = false;
+    }
     private void BindFuncs()
     {
         _mainInputAction.Player.DropItem.performed += DropItemCallback;
