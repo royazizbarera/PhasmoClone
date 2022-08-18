58f3a9eac53e97d058c61472e6f4ca8371999ac2
Maxx31 <maxym.619@ukr.net>
2022-08-18 22:08:56 +0300

    Финальный вариант дверей

--- Assets/JunkTemp/Scripts/DoorDraggable.cs
+++ Assets/JunkTemp/Scripts/DoorDraggable.cs
@@ -3,21 +3,18 @@ using Infrastructure;
 using Managers.Services;
 using UnityEngine;
 
-public class DoorDragable : MonoBehaviour, IDraggable
+public class DoorDraggable : MonoBehaviour, IDraggable
 {
 
     [SerializeField]
-    private float _forceAmmount = 30f;
+    private float _forceAmmount = 15f;
+    [SerializeField]
+    private float _distance = 3f;
 
     private Camera _cam;
-
     private bool _isInterracting = false;
-
-    private float _mouseX, _mouseY;
-    private Vector3 _pushDir, _direction;
     private Rigidbody _rb;
 
-    private InputSystem _input;
     void Awake()
     {
         _cam = Camera.main;
@@ -27,23 +24,25 @@ public class DoorDragable : MonoBehaviour, IDraggable
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _input = AllServices.Container.Single<InputSystem>();
     }
 
     void FixedUpdate()
     {
         if (_isInterracting)
         {
-            _mouseX = _input.CameraAxis.x;
-            _mouseY = _input.CameraAxis.y;
-
-            _pushDir = _mouseX * _cam.transform.right - _mouseY * _cam.transform.right;
-            //_direction = transform.position - 2 * transform.right;
-           // _rb.AddForceAtPosition(20 * _pushDir, _direction);
-           _rb.AddForce(_forceAmmount * _pushDir);;
+            DraggDoor();
         }
     }
 
+
+    private void DraggDoor()
+    {
+        Ray playerAim = _cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Vector3 nextPos = _cam.transform.position + playerAim.direction * _distance;
+        Vector3 currPos = transform.position;
+        _rb.velocity = (nextPos - currPos) * _forceAmmount;
+
+    }
     public void OnDragBegin()
     {
         _isInterracting = true;
