213d04948cc96d79ebe1f648c443c1beff489818
Maxx31 <maxym.619@ukr.net>
2022-08-20 14:12:03 +0300

    Взаимодействие призрака с предметами

--- Assets/Scripts/Ghosts/EnvIneraction/GhostItemInteraction.cs
+++ Assets/Scripts/Ghosts/EnvIneraction/GhostItemInteraction.cs
@@ -8,9 +8,11 @@ namespace Ghosts.EnvIneraction
 {
     public class GhostItemInteraction : MonoBehaviour
     {
-
         [SerializeField]
         private float _itemsThrowRadius;
+        [SerializeField]
+        private float _itemsThrowPower;
+
 
         private void Start()
         {
@@ -23,7 +25,7 @@ namespace Ghosts.EnvIneraction
             while (true)
             {
                 InteractWithIPickupable();
-                yield return new WaitForSeconds(3f);
+                yield return new WaitForSeconds(2f);
             }
 
         }
@@ -32,14 +34,22 @@ namespace Ghosts.EnvIneraction
         private void InteractWithIPickupable()
         {
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, _itemsThrowRadius);
-            Debug.Log("Check for interact");
             for(int i = 0; i< hitColliders.Length;i++)
             {
-                if(hitColliders[i].GetComponent<IPickupable>() != null)
+                Pickupable item = hitColliders[i].GetComponent<Pickupable>();
+                if (item != null)
                 {
-                    Debug.Log("Find " + hitColliders[i].name);
+                    DropItemRb(item);
                 }
             }
+        }
+
+        private void DropItemRb(IPickupable item)
+        {
+            Rigidbody itemRB = item.gameObject.GetComponent<Rigidbody>();
+            Vector3 randomItemDir = new Vector3(Random.Range(-1f, 1f), Random.Range(0.5f, 1f), Random.Range(-1f, 1f));
+
+            itemRB.AddForce(randomItemDir * _itemsThrowPower, ForceMode.VelocityChange);
         }
     }
 }
