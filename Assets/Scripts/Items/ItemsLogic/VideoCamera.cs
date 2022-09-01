e6ff979bc81c66bd24f07c736501b9ab9290f0e0
Riko211 <nikitosqwerty43@gmail.com>
2022-09-01 18:32:55 +0300

    Камеры и фотики

--- Assets/Scripts/Items/ItemsLogic/VideoCamera.cs
+++ Assets/Scripts/Items/ItemsLogic/VideoCamera.cs
@@ -5,9 +5,41 @@ namespace Items.ItemsLogic
 {
     public class VideoCamera : MonoBehaviour, IMainUsable
 {
+        [SerializeField] private Material _renderTextureMat;
+        [SerializeField] private GameObject _camera;
+        [SerializeField] private GameObject _cameraGreen;
+
+        private bool _normalMode = true;
+
+        private void Start()
+        {
+            SetNormalMode();
+        }
         public void OnMainUse()
         {
-            
+            if (_normalMode)
+            {
+                SetGhostOrbMode();
+            }
+            else
+            {
+                SetNormalMode();
+            }
+        }
+
+        private void SetNormalMode()
+        {
+            _renderTextureMat.color = Color.white;
+            _cameraGreen.SetActive(false);
+            _camera.SetActive(true);
+            _normalMode = true;
+        }
+        private void SetGhostOrbMode()
+        {
+            _renderTextureMat.color = Color.green;
+            _camera.SetActive(false);
+            _cameraGreen.SetActive(true);
+            _normalMode = false;
         }
     }
 }
