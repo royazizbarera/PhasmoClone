716196cb742edb929ef3122b46b068ea94a57947
Riko211 <nikitosqwerty43@gmail.com>
2022-09-01 14:41:46 +0300

    VideoCamera + GhostOrbs

---
+++ Assets/Scripts/Items/ItemsLogic/VideoCamera.cs
@@ -0,0 +1,13 @@
+using Items.Logic;
+using UnityEngine;
+
+namespace Items.ItemsLogic
+{
+    public class VideoCamera : MonoBehaviour, IMainUsable
+{
+        public void OnMainUse()
+        {
+            
+        }
+    }
+}
