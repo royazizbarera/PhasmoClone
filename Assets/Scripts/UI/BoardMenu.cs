a1af6ee7019a45f5fad1f6ef25120b5df796224c
Riko211 <nikitosqwerty43@gmail.com>
2022-09-18 17:00:37 +0300

    Доска в лобби

--- Assets/Scripts/UI/BoardMenu.cs
+++ Assets/Scripts/UI/BoardMenu.cs
@@ -9,16 +9,48 @@ namespace UI
 {
     public class BoardMenu : MonoBehaviour
     {
-        private BoardClickable _board;
+        private InputSystem _inputSystem;
+
+        [SerializeField] private GameObject _mainScreen;
+
+        private GameObject _currentScreen;
+        private GameObject _previousScreen;
 
         private void Start()
         {
-            _board = GetComponent<BoardClickable>();
+            _inputSystem = AllServices.Container.Single<InputSystem>();
+
+            LoadStartScreen();
+        }
+
+        public void OpenScreen(GameObject screen)
+        {
+            _currentScreen.SetActive(false);
+            screen.SetActive(true);
+            _previousScreen = _currentScreen;
+            _currentScreen = screen;
+        }
+        public void BackToPreviousScreen()
+        {
+            _currentScreen.SetActive(false);
+            _previousScreen.SetActive(true);
+
+            GameObject t = _previousScreen;
+
+            _previousScreen = _currentScreen;
+            _currentScreen = t;
+
         }
         public void LoadLevel()
         {
-            _board.UnlockInputControl();
+            _inputSystem.UnLockControl();
             GameBootstrapper.Instance.StateMachine.Enter<LoadLevelState>();
+        }
+
+        private void LoadStartScreen()
+        {
+            _currentScreen = _mainScreen;
+            _mainScreen.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/UI/LevelResultsScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+
+public class LevelResultsScreen : MonoBehaviour
+{
+    [SerializeField] private BoardMenu _boardMenu;
+
+    [SerializeField] private GameObject _resultScreen;
+
+    void Start()
+    {
+
+    }
+}
