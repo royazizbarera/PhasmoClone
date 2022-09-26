c542f9110eb3053f1009eedc36fd401648380e09
Maxx31 <maxym.619@ukr.net>
2022-09-26 14:16:10 +0300

    Smudge Sticke Parts

---
+++ Assets/Scripts/Items/ItemsLogic/SmudgeStick.cs
@@ -0,0 +1,15 @@
+using Items.Logic;
+using System.Collections;
+using UnityEngine;
+
+namespace Items.ItemsLogic
+{
+    public class SmudgeStick : MonoBehaviour, IMainUsable
+    {
+
+        public void OnMainUse()
+        {
+            
+        }
+    }
+}
