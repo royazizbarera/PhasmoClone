5ffb8e7b78a686827d422854d0e3174971f41317
Riko211 <nikitosqwerty43@gmail.com>
2022-10-06 14:14:14 +0300

    Звук дверей фургона

--- Assets/Scripts/Environment/TruckDoorButton.cs
+++ Assets/Scripts/Environment/TruckDoorButton.cs
@@ -5,18 +5,33 @@ public class TruckDoorButton : MonoBehaviour, IClickable
 {
     [SerializeField] private Animator _anim;
 
+    [SerializeField] private AudioSource _doorAudio;
+    [SerializeField] private AudioSource _engineAudio;
+
     private bool _isDoorsOpened = false;
     public void OnClick()
     {
         if (!_isDoorsOpened && !_anim.GetCurrentAnimatorStateInfo(0).IsName("ClosingDoors"))
         {
+            PlayDoorsSound();
             _anim.SetTrigger("OpenDoors");
             _isDoorsOpened = true;
         }
         else if (_isDoorsOpened && !_anim.GetCurrentAnimatorStateInfo(0).IsName("OpeningDoors"))
         {
+            PlayDoorsSound();
+            Invoke(nameof(PlayEngineSound), 2f);
             _anim.SetTrigger("CloseDoors");
             _isDoorsOpened = false;
         }      
+    }
+
+    private void PlayDoorsSound()
+    {
+        _doorAudio.Play();
+    }
+    private void PlayEngineSound()
+    {
+        _engineAudio.Play();
     }
 }
