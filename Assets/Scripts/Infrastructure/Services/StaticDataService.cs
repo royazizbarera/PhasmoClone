c6e07873696becf14e58a5a9c28b6dc85d91fa15
Maxx31 <maxym.619@ukr.net>
2022-10-15 15:54:29 +0300

    Улучшил атаку

--- Assets/Scripts/Infrastructure/Services/StaticDataService.cs
+++ Assets/Scripts/Infrastructure/Services/StaticDataService.cs
@@ -33,8 +33,8 @@ namespace Infrastructure.Services
 
         public GhostDataSO GetRandomGhost()
         {
-           // return ForMonster(_ghosts.ElementAt(Random.Range(0, _ghosts.Count)).Key);
-            return ForMonster(_ghosts.ElementAt(3).Key);
+            return ForMonster(_ghosts.ElementAt(Random.Range(0, _ghosts.Count)).Key);
+           // return ForMonster(_ghosts.ElementAt(1).Key);
         }
 
         public GhostDataSO ForMonster(string name) =>
