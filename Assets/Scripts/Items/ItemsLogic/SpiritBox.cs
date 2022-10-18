538b4c9922ec292852e7584a235361cce29b3e5a
Riko211 <nikitosqwerty43@gmail.com>
2022-10-18 17:34:30 +0300

    Доделал рацию

--- Assets/Scripts/Items/ItemsLogic/SpiritBox.cs
+++ Assets/Scripts/Items/ItemsLogic/SpiritBox.cs
@@ -1,10 +1,16 @@
+using GameFeatures;
+using Infrastructure;
+using Infrastructure.Services;
 using Items.Logic;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utilities.Constants;
 
-public class SpiritBox : MonoBehaviour, IMainUsable
+public class SpiritBox : MonoBehaviour, IMainUsable, IDisababled, IDroppable
 {
+    [SerializeField]
+    private GameObject _mesh;
     [SerializeField]
     private AudioSource _radioFrequency;
     [SerializeField]
@@ -12,8 +18,36 @@ public class SpiritBox : MonoBehaviour, IMainUsable
     [SerializeField]
     private float _ghostVoiceVolume = 0.3f;
 
-    private WaitForSeconds WaitForSecond = new WaitForSeconds(1f); 
+    [SerializeField]
+    private RoomIdentifire _currRoom;
+    [SerializeField]
+    private float _defaultChanceToSay = 1f;
+    [SerializeField]
+    private float _increaseChancePerTick = 0.1f;
+    [SerializeField]
+    private float _tickTime = 3f;
+    [SerializeField]
+    private float _inHandModifier = 15f;
+
+    private LevelSetUp _levelSetUp;
+    private GhostInfo _ghostInfo;
+    private LevelRooms.LevelRoomsEnum _ghostRoom;
+
+    private float _curChanceToSay = 0f;
+
     private bool _isEnabled = false;
+    private bool _isInHands = false;
+    private bool _hasSpiritBoxEvidence = false;
+
+    private void Start()
+    {
+        _levelSetUp = AllServices.Container.Single<LevelSetUp>();
+        _ghostRoom = _levelSetUp.CurrGhostRoom;
+
+        if (_ghostRoom == LevelRooms.LevelRoomsEnum.NoRoom) _levelSetUp.OnLevelSetedUp += SetUpInfo;
+
+        _curChanceToSay = _defaultChanceToSay;
+    }
     public void OnMainUse()
     {
         if (_isEnabled == false) EnableSpiritBox();
@@ -24,33 +58,59 @@ public class SpiritBox : MonoBehaviour, IMainUsable
     {
         while (true)
         {
-            yield return WaitForSecond;
-            yield return WaitForSecond;
-            yield return WaitForSecond;
-            yield return WaitForSecond;
-            yield return WaitForSecond;
-
-            _radioFrequency.PlayOneShot(_ghostVoiceClips[Random.Range(0,_ghostVoiceClips.Length)], _ghostVoiceVolume);
+            yield return new WaitForSeconds(_tickTime);
+            if (_currRoom.CurrRoom == _ghostRoom)
+            {
+                if (CalculateChanceToSay(_curChanceToSay, _isInHands, _inHandModifier))
+                {
+                    _radioFrequency.PlayOneShot(_ghostVoiceClips[Random.Range(0, _ghostVoiceClips.Length)], _ghostVoiceVolume);
+                    _curChanceToSay = _defaultChanceToSay;
+                }
+                else _curChanceToSay += _increaseChancePerTick;
+            }
         }
     }
-    private void OnEnable()
+
+    private bool CalculateChanceToSay(float chance, bool inHand, float modifier)
     {
-        if (_isEnabled) EnableSpiritBox();
+        float number = Random.Range(0f, 100f);
+        if (inHand) chance *= modifier;
+        if (number <= chance) return true;
+        else return false;
     }
-    private void OnDisable()
+    private void SetUpInfo()
     {
-        StopCoroutine(nameof(DetectGhostVoice));
+        _ghostInfo = _levelSetUp.GhostInfo;
+        _ghostRoom = _levelSetUp.CurrGhostRoom;
+        if (_ghostInfo.GhostData.GhostEvidences.Contains(GhostEvidence.GhostEvidencesTypes.SpiritBox)) _hasSpiritBoxEvidence = true;
     }
     private void EnableSpiritBox()
     {
         _isEnabled = true;
         _radioFrequency.Play();
-        StartCoroutine(nameof(DetectGhostVoice));
+        if (_hasSpiritBoxEvidence) StartCoroutine(nameof(DetectGhostVoice));
     }
     private void DisableSpiritBox()
     {
         _isEnabled = false;
         _radioFrequency.Pause();
         StopCoroutine(nameof(DetectGhostVoice));
+    }
+    public void DisableItem()
+    {
+        _mesh.SetActive(false);
+        _radioFrequency.Pause();
+        StopCoroutine(nameof(DetectGhostVoice));
+    }
+    public void EnableItem()
+    {
+        _isInHands = true;
+        _mesh.SetActive(true);
+        if (_isEnabled) EnableSpiritBox();
+    }
+
+    public void DropItem()
+    {
+        _isInHands = false;
     }
 }
