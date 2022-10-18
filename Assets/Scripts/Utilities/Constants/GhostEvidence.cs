1ebe3737f730669a68f304ada4106528e50a4160
Riko211 <nikitosqwerty43@gmail.com>
2022-10-18 15:14:32 +0300

    Добавил рацию в журнал

--- Assets/Scripts/Utilities/Constants/GhostEvidence.cs
+++ Assets/Scripts/Utilities/Constants/GhostEvidence.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 
 public static class GhostEvidence
 {
-    public enum GhostEvidencesTypes { FreezingTemps, EMF5, Fingerprints, GhostWriting, GhostOrbs };
+    public enum GhostEvidencesTypes { FreezingTemps, EMF5, Fingerprints, GhostWriting, GhostOrbs, SpiritBox };
 }
