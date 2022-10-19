666573de0d9b9f9dbbb464523cc9cb74c6ba768c
Riko211 <nikitosqwerty43@gmail.com>
2022-10-19 18:14:02 +0300

    Мигание света

--- Assets/Scripts/Environment/LightButton.cs
+++ Assets/Scripts/Environment/LightButton.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Items.Logic;
 using System;
+using System.Collections;
 
 [RequireComponent(typeof(Collider))]
 public class LightButton : MonoBehaviour, IClickable
@@ -23,9 +24,16 @@ public class LightButton : MonoBehaviour, IClickable
     [SerializeField]
     private GameObject _sparkParticles;
 
+    private float[] _startLightPower;
+
     private AudioSource _audioSource;
     private bool _isExploded = false;
     private bool _ghostAttack = false;
+
+    private float _flickTime = 0.3f;
+    private int _stepsCount = 10;
+    private float _change;
+
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
@@ -36,6 +44,13 @@ public class LightButton : MonoBehaviour, IClickable
         else
         {
             DisableAllLights();
+        }
+
+
+        _startLightPower = new float[_connectedLights.Length];
+        for (int i = 0; i < _connectedLights.Length; i++)
+        {
+            _startLightPower[i] = _connectedLights[i].intensity;
         }
     }
     public void OnClick()
@@ -66,15 +81,51 @@ public class LightButton : MonoBehaviour, IClickable
     }
     public void GhostAttackOffLight()
     {
-        DisableAllLights();
-        _onLightOff?.Invoke();
-        _isEnabled = false;
+        //DisableAllLights();
+        //_onLightOff?.Invoke();
+        //_isEnabled = false;
+        StartCoroutine(nameof(LightFlick));
         _ghostAttack = true;
     }
     public void EndGhostAttack()
     {
+        StopCoroutine(nameof(LightFlick));
+        for (int i = 0; i < _connectedLights.Length; i++)
+        {
+            _connectedLights[i].intensity = _startLightPower[i];
+        }
+        DisableAllLights();
+        _isEnabled = false;
         _ghostAttack = false;
     }
+
+    IEnumerator LightFlick()
+    {
+        while (true)
+        {
+            _flickTime = UnityEngine.Random.Range(0.25f, 0.4f);
+            yield return new WaitForSeconds(_flickTime);
+            if (_isEnabled)
+            {
+                float newLightPower = _startLightPower[0] * UnityEngine.Random.Range(0f, 0.2f);
+                _change = (_connectedLights[0].intensity - newLightPower) / _stepsCount;
+                StartCoroutine(nameof(ChangeLightPower));
+            }
+        }
+    }
+    IEnumerator ChangeLightPower()
+    {
+        for (int i = 0; i < _stepsCount; i++)
+        {
+            foreach (Light light in _connectedLights)
+            {
+                light.intensity -= _change;
+            }
+
+            yield return new WaitForSeconds(_flickTime / _stepsCount);
+        }
+    }
+
     public bool CheckIfEnabled()
     {
         return _isEnabled;
